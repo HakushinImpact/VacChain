137acfacac7051f2e25a499fe1a150bce9b209d3
GravitonXD <71811757+GravitonXD@users.noreply.github.com>
2021-08-26 14:53:51 +0800

    Initial Login.vue
    Added Input Fields:
    1. Username
    2. Password
    
    Added Button
    1. Login

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="column is-half is-offset-one-quarter">
+        <h1 class="title"> VacChain </h1>
+
+        <form>
+            <div class="field">
+                <p class="control has-icons-left has-icons-right">
+                    <input v-model="username" class="input" type="text" placeholder="Username">
+                    <span class="icon is-small is-left">
+                        <FontAwesomeIcon icon="user" />
+                    </span>
+                </p>
+            </div>
+
+            <div class="field">
+                <p class="control has-icons-left">
+                    <input  v-model="password" class="input" type="password" placeholder="Password">
+                    <span class="icon is-small is-left">
+                        <FontAwesomeIcon icon="lock" />
+                    </span>
+                </p>
+            </div>
+            
+            <div class="field">
+                <p class="control">
+                    <button class="button is-success" :disabled="!isComplete">
+                    Login
+                    </button>
+                </p>
+            </div>
+
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data () {
+        return {
+            username: '',
+            password: '',
+            isComplete: false,
+            valid: false
+        }
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped>
+</style>
