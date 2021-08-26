a5ae0e9c5ffde40fdccba62c28c8f29db9a7e69f
Stick1000 <janleymolina@outlook.com>
2021-08-26 20:29:00 +0800

    Added Not Found page

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    404
+    <router-link :to="{ name: 'Login' }" class="button is-primary is-rounded">
+      Login
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Not Found"
+};
+</script>
+
+<style>
+</style>
