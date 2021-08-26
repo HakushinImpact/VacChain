7af203c3f588e57f565cd94d8616665cb03ba2a6
GravitonXD <71811757+GravitonXD@users.noreply.github.com>
2021-08-27 00:32:43 +0800

    Update Manage.vue

--- src/components/Manage.vue
+++ src/components/Manage.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>hello</div>
+  <div class="column is-4 is-offset-4 my-auto">
+    <div>
+          <input
+            class="input is-rounded"
+            type="text"
+            placeholder="Search Record"
+          />
+          <span class="icon is-small is-left">
+            <FontAwesomeIcon icon="search" />
+          </span>
+        </p>
+    </div>
+  </div>
 </template>
 
 <script>
