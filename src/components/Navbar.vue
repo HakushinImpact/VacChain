fb7daadd5475a758ee085f0fb55e3b6c3dc30e0b
GravitonXD <71811757+GravitonXD@users.noreply.github.com>
2021-08-28 01:00:54 +0800

    Minor Update to NavBar
    Added Logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
+      <a class="navbar-item">
         <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
+          src="../assets/logo.png"
+          height="100%"
         />
+        <p class="title is-6"> VacChain </p>
       </a>
 
       <a
