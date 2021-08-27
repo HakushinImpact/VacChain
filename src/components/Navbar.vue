872e809cea98e222ae43b580ae1fa8c0f1f1f835
Stick1000 <janleymolina@outlook.com>
2021-08-28 02:50:58 +0800

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar mb-5" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item">
         <img src="../assets/logo.png" height="100%" />
