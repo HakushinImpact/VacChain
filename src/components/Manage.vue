8199f504624a4f950b4a794dd18666861bd67206
Stick1000 <janleymolina@outlook.com>
2021-08-28 00:53:52 +0800

    Rudimentary implementation of Manage page

--- src/components/Manage.vue
+++ src/components/Manage.vue
@@ -1,49 +1,48 @@
 <template>
   <div class="container mt-6">
     <div class="columns is-centered">
-        <div class="column is-three-fifths">
-          <div class="mb-6">
-            <p class="control has-icons-left">
-              <input
-                v-model="search"
-                class="input is-rounded"
-                type="text"
-                placeholder="Search Record"
-              />
-              <span class="icon is-small is-left">
-                <FontAwesomeIcon icon="search" />
-              </span>
-            </p>
-          </div>
+      <div class="column is-three-fifths">
+        <div class="mb-6">
+          <p class="control has-icons-left">
+            <input
+              v-model="search"
+              class="input is-rounded"
+              type="text"
+              placeholder="Search Record"
+            />
+            <span class="icon is-small is-left">
+              <FontAwesomeIcon icon="search" />
+            </span>
+          </p>
+        </div>
       </div>
     </div>
-    <!-- <VaccineeCard
+    <VaccineeCard
       v-for="(vaccinee, index) in vaccinees"
-      :key="index", vaccinee="vaccinee"
-    /> -->
-    <div v-for="(item, index) in haha" :key="index" class="columns is-centered">
-      <div class="column is-7">
-        <VaccineeCard @click-entry="record"/>
-      </div>
-    </div>
+      :key="index"
+      :vaccinee="vaccinee"
+    />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import VaccineeCard from './VaccineeCard.vue';
 
 export default {
   name: 'Manage',
-  props: {
-    vaccinees: {}
-  },
   components: {
     VaccineeCard
   },
   data() {
     return {
-      haha: ['1', '2', '3']
+      vaccinees: null
     };
+  },
+  created() {
+    axios.get('http://localhost:5000/getEntries').then(response => {
+      this.vaccinees = response.data;
+    });
   },
   methods: {
     record(event) {
--- src/components/VaccineeCard.vue
+++ src/components/VaccineeCard.vue
@@ -8,10 +8,18 @@
       </div>
       <div class="column">
         <div>
-          <!-- <p class="title is-4">{{vaccinee.personal_info.first_name}} {{vaccinee.personal_info.middle_name}} {{vaccinee.personal_info.last_name}} {{vaccinee.personal_info.suffix}}</p>
-          <p class="subtitle is-6">Vaccination Status: Vaccinated ({{vaccinee.vaccine_info.dosage}} Dose)</p> -->
-          <p class="title is-4">John Smith</p>
-          <p class="subtitle is-6">Vaccination Status: Vaccinated (1st Dose)</p>
+          <p class="title is-4">
+            {{ vaccinee.personal_info.first_name }}
+            {{ vaccinee.personal_info.middle_name }}
+            {{ vaccinee.personal_info.last_name }}
+            {{ vaccinee.personal_info.suffix }}
+          </p>
+          <p class="subtitle is-6">
+            Vaccination Status: Vaccinated ({{
+              vaccinee.vaccine_info.dosage
+            }}
+            Dose)
+          </p>
         </div>
       </div>
     </div>
