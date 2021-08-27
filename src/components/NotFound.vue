1f5aa2db11257534494859d1ab542a540b74e128
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-08-27 14:39:51 +0800

    Added Not Found

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    404
-    <router-link :to="{ name: 'Login' }" class="button is-primary is-rounded">
-      Login
-    </router-link>
+  <div class="container">
+    <div class="columns is-centered">
+      <img src="../assets/404_not_found.png" />
+    </div>
+    <div class="has-text-centered">
+      <p class="title">OOPS... Page not Found!</p>
+      <div>
+        <router-link :to="{ name: 'Login' }" class="button is-primary is-rounded">
+        Login
+      </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/VaccRegForm.vue
+++ src/components/VaccRegForm.vue
@@ -17,7 +17,6 @@ export default {
   },
   data() {
     return {
-      view: PersonalInformationForm,
       show: true
     }
   },
