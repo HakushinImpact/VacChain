4970aff8b64b7947932c8c8ae6eaf412c0b7b9e3
Stick1000 <janleymolina@outlook.com>
2021-08-28 02:43:34 +0800

    Revert "Added logo"
    This reverts commit 71c6291567e2818583b1d3826d86b8a991669f71.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,11 @@
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item">
-        <img src="../assets/logo.svg" height="100%" />
-        <p class="title is-6">VacChain</p>
+        <img
+          src="../assets/logo.png"
+          height="100%"
+        />
+        <p class="title is-6"> VacChain </p>
       </a>
 
       <a
