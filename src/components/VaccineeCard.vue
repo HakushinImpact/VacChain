a1b3f430d10ea54a32aa70784a64473700496e4a
Hezzz <49769113+Hezzz@users.noreply.github.com>
2021-08-27 17:16:36 +0800

    Manage Page and Vaccinee Card Info

---
+++ src/components/VaccineeCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="box">
+        <div class="columns is-vcentered">
+            <div class="column has-text-centered is-3">
+                <span class="icon is-large fa-5x">
+                    <FontAwesomeIcon icon="user"/>
+                </span>
+            </div>
+            <div class="column">
+                <div>
+                    <p class="title is-4">John Smith</p>
+                    <p class="subtitle is-6">Vaccination Status: Vaccinated (1st Dose)</p>
+                </div>
+                <div class="mt-2">
+                    <p class="is-size-6">Address: Somewhere in the Woods</p>
+                    <p class="is-size-6">Contact Number: 09123456789</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VaccineeCard',
+}
+</script>
+
+<style>
+
+</style>
